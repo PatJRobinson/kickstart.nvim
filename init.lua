57518c79829b76057cb9359c1edbec1e12185b47
PatJRobinson <robinsonpaddy4@gmail.com>
2025-11-15 20:25:34 +0000

    tidy lsps (no Mason)

--- init.lua
+++ init.lua
@@ -826,42 +826,13 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       --local ensure_installed = vim.tbl_keys(servers or {})
 
-      -- build a list of servers we *do* want mason-tool-installer to ensure, EXCLUDING clangd
-      local ensure_installed = {}
-      for name, _ in pairs(servers or {}) do
-        if name ~= 'clangd' and name ~= 'asm_lsp' and name ~= 'lua_ls' then
-          table.insert(ensure_installed, name)
-        end
-      end
-
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-      require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-        automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
-      }
-      -- Ensure servers excluded from mason (e.g. clangd) are still registered with lspconfig
+      -- register lsps with language lspconfig
       -- Put this immediately after your require('mason-lspconfig').setup { ... } call.
       local lspconfig = require 'lspconfig'
       for name, cfg in pairs(servers or {}) do
-        if name == 'clangd' or name =='asm_lsp' or name == 'lua_ls' then
-          -- Merge capabilities the same way the handler would
-          cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
-          lspconfig[name].setup(cfg)
-        end
+        -- Merge capabilities the same way the handler would
+        cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
+        lspconfig[name].setup(cfg)
       end
     end,
   },
