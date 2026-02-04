6dcc9b42be66db482fa7e7716e8a3cbe11c83361
PatJRobinson <robinsonpaddy4@gmail.com>
2026-02-04 22:19:49 +0000

    wip debug clangd lsp

--- init.lua
+++ init.lua
@@ -261,41 +261,6 @@ require('lazy').setup({
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
   {
-    "epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
-    lazy = true,
-    ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
-    dependencies = {
-      -- Required.
-      "nvim-lua/plenary.nvim",
-
-      -- see below for full list of optional dependencies 👇
-    },
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "~/vaults/personal",
-        },
-        {
-          name = "work",
-          path = "~/vaults/work",
-        },
-      },
-
-      -- see below for full list of options 👇
-    },
-  },
-
-  {
     "mikavilpas/yazi.nvim",
     version = "*", -- use the latest stable version
     event = "VeryLazy",
@@ -861,7 +826,6 @@ require('lazy').setup({
             '--clang-tidy',
             '--completion-style=detailed',
             '--header-insertion=never',
-            '--query-driver=/nix/store/*-clang-wrapper-*/bin/*',
           },
           flags = lsp_flags,
 
@@ -970,6 +934,7 @@ require('lazy').setup({
       for name, cfg in pairs(servers or {}) do
         -- Merge capabilities the same way the handler would
         cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
+        cfg.autostart = true
         vim.lsp.config(name, cfg)
         vim.lsp.enable(name)
       end
